7b63d6cafc98c7553401ec9a8af4d232243a544f
Willy Cedric <w.dongmo@gmail.com>
2021-10-29 15:09:37 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <p>Hello</p>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
